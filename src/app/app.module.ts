dcaa674a6444c538d314ed3ecc929d4b7e4d35ad
Osman Mazariegos <osman.mazariegosmendez@gmail.com>
2018-03-09 15:59:34 -0600

    performance 100

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatCardModule  } from '@angular/material';
+import { MatToolbarModule, MatCardModule, MatButtonModule  } from '@angular/material';
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,8 +13,10 @@ import { ImgCardComponent } from './img-card/img-card.component';
   ],
   imports: [
     BrowserModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
